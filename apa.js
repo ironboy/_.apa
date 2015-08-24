12bb56128366684a5c6fb3e9a2feb896c8f1eb19
Thomas Frank <ironboy@users.noreply.github.com>
2015-08-24 22:01:04 +0200

    Update apa.js

--- apa.js
+++ apa.js
@@ -17,7 +17,7 @@
   Please Note: On by default after loading this library!
 
   Wrap underscore methods as array methods
-  for those of us who thinks it is ok to extend the Array.prototype
+  for those of us who think it is ok to extend the Array.prototype
   for sweeter syntax - but with the option to turn this on/off
 
   EXAMPLES:
